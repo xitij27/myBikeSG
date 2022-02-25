59b1f728518aa1a7abed0aa3f9e5741235cdc7dc
Harsh Rao D <88545952+HarshRaoD@users.noreply.github.com>
2022-02-25 17:21:59 +0800

    AccountManager file (incomplete)
    Done to test pull requests

---
+++ Backend-Coding/AccountManager.js
@@ -0,0 +1,8 @@
+// The attributes in UML Class Diagram
+var accs = {};
+// How to use a dictionary in js: https://pietschsoft.com/post/2015/09/05/javascript-basics-how-to-create-a-dictionary-with-keyvalue-pairs
+var timeUsed = {};
+var failedLogins = {};
+var OTPs = {};
+const ACCS_MAX_LEN = 100;
+
