fdaecfdf0612ad6e9bbc8d22ae0a3e6d092175bc
Harsh Rao D <88545952+HarshRaoD@users.noreply.github.com>
2022-02-25 18:25:34 +0800

    Add files via upload

--- Backend-Coding/AccountManager.js
+++ Backend-Coding/AccountManager.js
@@ -6,3 +6,51 @@ var failedLogins = {};
 var OTPs = {};
 const ACCS_MAX_LEN = 100;
 
+function Login(email, password)
+{
+     /**
+         * Returns 1 is password is correct
+         * Returns 2 if account is disabled
+         * Returns 0 if password is incorrect
+         * Returns -1 if account does not exist
+         * @param: {String} password The password to check
+         */
+    if(email in accs)
+        return accs[email].checkPassword(password);
+    else
+        return -1;
+}
+
+function resetPassword(email, otp, password)
+{
+    if(!(OTPs[email].checkOTP(otp)))
+    {
+        return false;
+    }
+    else
+    {
+        accs[email].updatePassword(password);
+        delete OTPs[email]
+        // Above code needs to do tested and may break
+        return true;
+    }
+}
+
+function generateOTP(email)
+{
+    OTPs[email] = new OTP(email);
+    // TODO send email to user with otp.
+}
+
+function signUp(email,password,DOB,Name,otp) 
+{ 
+    accs[email] = new Account(email,DOB,Name); 
+    OTPs[email] = new OTP(email); 
+    OTPs[email].generateOTP(email); 
+    chk = OTPs[email].checkOTP(otp); 
+    if(chk == true) 
+    { 
+        accs.set(email, obj); 
+    } 
+}
+
