0eeb3402e895cf4fba3a52164ae159f780d976e8
Darryl Ng <51407026+ddaarrrryyll@users.noreply.github.com>
2022-03-29 00:23:28 +0800

    plotted repair shops location together with infoheader

--- mybikesg/src/components/Drawer.js
+++ mybikesg/src/components/Drawer.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import * as FaIcons from "react-icons/fa";
 import * as GrIcons from "react-icons/gr";
 import { Link } from "react-router-dom";
@@ -27,48 +27,48 @@ const Drawer = ({ onSend }) => {
         <>
             <div className={burger ? "burger" : "not-burger"}>
                 <Link to="#" className="menu-bars">
-                    <FaIcons.FaBars onClick={showDrawer}/>
+                    <FaIcons.FaBars onClick={showDrawer} />
                 </Link>
             </div>
             <nav className={burger ? "nav-menu active" : "nav-menu"}>
                 <ul className='nav-menu-items'>
                     <li className="navbar-toggle">
                         <Link to="#" className='menu-bars'>
-                            <GrIcons.GrClose onClick={showDrawer}/>
+                            <GrIcons.GrClose onClick={showDrawer} />
                         </Link>
                     </li>
                     <li>
-                    <form className="add-form" onSubmit={onSubmit}>
-                        <div className="form-control">
-                            <label className="form-control-header">Starting Location</label>
-                            <input 
-                            type="text" 
-                            placeholder="Starting Location" 
-                            value={start} 
-                            onChange={(e)=>setStart(e.target.value)}/>
-                        </div>
-                        <div className="form-control">
-                            <label className="form-control-header">Ending Location</label>
-                            <input 
-                            type="text" 
-                            placeholder="Ending Location" 
-                            value={dest} 
-                            onChange={(e)=>setDest(e.target.value)}/>
-                        </div>
-                        <div className="form-control form-control-check">
-                            <label className="form-control-header">Set nearest racks as destination</label>
-                            <input 
-                            type="checkbox"
-                            checked={rack}
-                            value={rack}
-                            onChange={(e)=>setRack(e.currentTarget.checked)}
-                            />
-                        </div>
-                        <input 
-                        type="submit" 
-                        value="Submit" 
-                        className="btn btn-block"/>
-                    </form>
+                        <form className="add-form" onSubmit={onSubmit}>
+                            <div className="form-control">
+                                <label className="form-control-header">Starting Location</label>
+                                <input
+                                    type="text"
+                                    placeholder="Starting Location"
+                                    value={start}
+                                    onChange={(e) => setStart(e.target.value)} />
+                            </div>
+                            <div className="form-control">
+                                <label className="form-control-header">Ending Location</label>
+                                <input
+                                    type="text"
+                                    placeholder="Ending Location"
+                                    value={dest}
+                                    onChange={(e) => setDest(e.target.value)} />
+                            </div>
+                            <div className="form-control form-control-check">
+                                <label className="form-control-header">Set nearest racks as destination</label>
+                                <input
+                                    type="checkbox"
+                                    checked={rack}
+                                    value={rack}
+                                    onChange={(e) => setRack(e.currentTarget.checked)}
+                                />
+                            </div>
+                            <input
+                                type="submit"
+                                value="Submit"
+                                className="btn btn-block" />
+                        </form>
                     </li>
                 </ul>
             </nav>
