1583876d1d9fcdcf48fec17888384587b11887ab
Darryl Ng <51407026+ddaarrrryyll@users.noreply.github.com>
2022-04-01 15:15:59 +0800

    added logic for toggling visibility for all markers

--- mybikesg/src/components/Main.js
+++ mybikesg/src/components/Main.js
@@ -19,6 +19,7 @@ import * as GrIcons from "react-icons/gr";
 import MapStyle from './MapStyle'
 import "./Drawer.css";
 import racks_lta_json from "../data/lta-bicycle-rack-geojson.json";
+import racks_user_json from "../data/users-bicycle-racks.json";
 import bike_repairs_json from "../data/bike_repair.json";
 
 const mapContainerStyle = {
@@ -37,6 +38,23 @@ const options = {
 
 const libraries = ["places"];
 
+// array to hold rack locations (LTA)
+var racks_lta_locs = []
+for (const rack_details of racks_lta_json.features) {
+    racks_lta_locs.push({ rack_details, "vis": true });
+}
+
+// array to hold rack locations (user supplied)
+var racks_user_locs = []
+for (const rack_details of racks_user_json) {
+    racks_user_locs.push({ rack_details, "vis": true });
+}
+
+// array to hold repair shop locations
+var repair_shop_locs = []
+for (const repair_details of bike_repairs_json) {
+    repair_shop_locs.push({ repair_details, "vis": true });
+}
 
 export function Main({ }) {
     // misc stuff
@@ -45,7 +63,8 @@ export function Main({ }) {
         libraries,
     });
 
-    const [selectedRack, setSelectedRack] = React.useState(null);
+    const [selectedLTARack, setSelectedLTARack] = React.useState(null);
+    const [selectedUserRack, setSelectedUserRack] = React.useState(null);
     const [selectedShop, setSelectedShop] = React.useState(null);
 
     const mapRef = React.useRef();
@@ -53,71 +72,126 @@ export function Main({ }) {
         mapRef.current = map;
     }, []);
 
-
     const panTo = React.useCallback(({ lat, lng }) => {
         mapRef.current.panTo({ lat, lng });
         mapRef.current.setZoom(14);
     }, []);
 
-    // array to hold rack locations (LTA)
-    var racks_lta_locs = []
-    for (const rack_details of racks_lta_json.features) {
-        racks_lta_locs.push({ rack_details, "vis": true });
+    // For nav bar toggle visibility
+    const [visOverall, toggleOverall] = useState(true)
+    const setOverall = () => {toggleOverall(!visOverall); plot_all()}
+    const [visRacks, toggleRacks] = useState(true)
+    const setRackVis = () => {toggleRacks(!visRacks)}
+    const [visRepairs, toggleRepairs] = useState(true)
+    const setRepairVis = () => toggleRepairs(!visRepairs)
+    const [visRoute, toggleRoute] = useState(true)
+    const setRouteVis = () => toggleRoute(!visRoute)
+
+    // only remove ever market if all are shown, otherwise show all markers
+    const plot_all = () => {
+        if (visRacks && visRepairs && visRoute) {
+            setRackVis()
+            setRepairVis()
+            setRouteVis()
+        }
+        // if any is set to false, we set to true and plot on map
+        if (!visRacks) setRackVis()
+        if (!visRepairs) setRepairVis()
+        if (!visRoute) setRouteVis();
     }
 
-
-    // array to hold rack locations (user supplied)
-    var racks_user_locs = []
-
-
-    // array to hold repair shop locations
-    var repair_shop_locs = []
-    for (const repair_details of bike_repairs_json) {
-        repair_shop_locs.push({ repair_details, "vis": true });
+    const plot_repair_shops = () => {
+        for (const repairShop of repair_shop_locs) {
+            if (visRepairs) {
+                repairShop.vis = true
+            } else repairShop.vis = false
+        }
     }
 
     // start and dest locations, {lat, lng}
     const [start, setStart] = useState({ lat: 0, lng: 0 });
     const [dest, setDest] = useState({ lat: 0, lng: 0 });
 
-    // this chunk is the logic for showing only markers in radius of start and dest
-    const range = 0.5 // 500m
-    var marker_lat, marker_lng, start_kx, start_dx, start_dy, dest_kx, dest_dx, dest_dy;
-    if (start && (start.lat !== 0 && start.lng !== 0)) {
-        // logic for lta racks, need to do one more for user added
-        for (const rack_lta_loc of racks_lta_locs) {
-            marker_lat = rack_lta_loc.rack_details.geometry.coordinates[1]
-            marker_lng = rack_lta_loc.rack_details.geometry.coordinates[0]
-            start_kx = Math.cos(Math.PI * start.lat / 180) * 111;
-            start_dx = Math.abs(start.lng - marker_lng) * start_kx;
-            start_dy = Math.abs(start.lat - marker_lat) * 111;
+    const plot_racks = () => {
+        if (visRacks && start && (start.lat !== 0 && start.lng !== 0)) {
+            const range = 0.5 // 500m
+            var marker_lat, marker_lng, start_kx, start_dx, start_dy, dest_kx, dest_dx, dest_dy;
+            // for lta racks
+            for (const rack_lta_loc of racks_lta_locs) {
+                // this chunk is the logic for showing only markers in radius of start and dest
+                marker_lat = rack_lta_loc.rack_details.geometry.coordinates[1]
+                marker_lng = rack_lta_loc.rack_details.geometry.coordinates[0]
+                start_kx = Math.cos(Math.PI * start.lat / 180) * 111;
+                start_dx = Math.abs(start.lng - marker_lng) * start_kx;
+                start_dy = Math.abs(start.lat - marker_lat) * 111;
 
-            // if dest is given
-            if (dest && (dest.lat !== 0 && dest.lng !== 0)) {
-                dest_kx = Math.cos(Math.PI * dest.lat / 180) * 111;
-                dest_dx = Math.abs(dest.lng - marker_lng) * dest_kx;
-                dest_dy = Math.abs(dest.lat - marker_lat) * 111;
-                // if marker distance > range, make it disappear
-                if ((Math.sqrt(start_dx * start_dx + start_dy * start_dy) > range) && (Math.sqrt(dest_dx * dest_dx + dest_dy * dest_dy) > range)) {
-                    rack_lta_loc.vis = false;
-                } else rack_lta_loc.vis = true;
+                // if dest is given
+                if (dest && (dest.lat !== 0 && dest.lng !== 0)) {
+                    dest_kx = Math.cos(Math.PI * dest.lat / 180) * 111;
+                    dest_dx = Math.abs(dest.lng - marker_lng) * dest_kx;
+                    dest_dy = Math.abs(dest.lat - marker_lat) * 111;
+                    // if marker distance > range, make it disappear
+                    if ((Math.sqrt(start_dx * start_dx + start_dy * start_dy) > range) && (Math.sqrt(dest_dx * dest_dx + dest_dy * dest_dy) > range)) {
+                        rack_lta_loc.vis = false;
+                    } else rack_lta_loc.vis = true;
+                }
+                // if only start is given
+                else {
+                    // if marker distance > range, make it disappear
+                    if (Math.sqrt(start_dx * start_dx + start_dy * start_dy) > range) {
+                        rack_lta_loc.vis = false;
+                    } else rack_lta_loc.vis = true;
+                }
             }
-            // if only start is given
-            else {
-                // if marker distance > range, make it disappear
-                if (Math.sqrt(start_dx * start_dx + start_dy * start_dy) > range) {
-                    rack_lta_loc.vis = false;
-                } else rack_lta_loc.vis = true;
+
+            // for user added racks
+            for (const rack_user_loc of racks_user_locs) {
+                
+                marker_lat = rack_user_loc.rack_details.Lat
+                marker_lng = rack_user_loc.rack_details.Lng
+                start_kx = Math.cos(Math.PI * start.lat / 180) * 111;
+                start_dx = Math.abs(start.lng - marker_lng) * start_kx;
+                start_dy = Math.abs(start.lat - marker_lat) * 111;
+
+                // if dest is given
+                if (dest && (dest.lat !== 0 && dest.lng !== 0)) {
+                    dest_kx = Math.cos(Math.PI * dest.lat / 180) * 111;
+                    dest_dx = Math.abs(dest.lng - marker_lng) * dest_kx;
+                    dest_dy = Math.abs(dest.lat - marker_lat) * 111;
+                    // if marker distance > range, make it disappear
+                    if ((Math.sqrt(start_dx * start_dx + start_dy * start_dy) > range) && (Math.sqrt(dest_dx * dest_dx + dest_dy * dest_dy) > range)) {
+                        rack_user_loc.vis = false;
+                    } else rack_user_loc.vis = true;
+                }
+                // if only start is given
+                else {
+                    // if marker distance > range, make it disappear
+                    if (Math.sqrt(start_dx * start_dx + start_dy * start_dy) > range) {
+                        rack_user_loc.vis = false;
+                    } else rack_user_loc.vis = true;
+                }
             }
+        }
+
+        else {
+            if (visRacks) {
+                for (const rack_lta_loc of racks_lta_locs) rack_lta_loc.vis = true
+                for (const rack_user_loc of racks_user_locs) rack_user_loc.vis = true
+            } else {
+                for (const rack_lta_loc of racks_lta_locs) rack_lta_loc.vis = false
+                for (const rack_user_loc of racks_user_locs) rack_user_loc.vis = false
+            }
+            
         }
     }
 
+    plot_repair_shops()
+    plot_racks();
 
     // helper functions for misc stuff
     const send_loc = (locs) => {
         console.log(locs)
         setDest(locs.dest);
-
     }
 
     const markStart = React.useCallback(({ lat, lng }) => {
@@ -151,7 +225,7 @@ export function Main({ }) {
                 <Marker position={{ lat: dest.lat, lng: dest.lng }} />
             }
 
-            {/* plot all the racks on the map */}
+            {/* plot all the lta racks on the map */}
             {racks_lta_locs.map(rack => (
                 <Marker
                     key={rack.rack_details.properties.Name}
@@ -163,7 +237,27 @@ export function Main({ }) {
                     }}
                     visible={rack.vis}
                     onClick={() => {
-                        setSelectedRack(rack)
+                        setSelectedLTARack(rack)
+                        setSelectedUserRack(null)
+                        setSelectedShop(null)
+                    }}
+                />
+            ))}
+
+            {/* plot all user racks on map */}
+            {racks_user_locs.map(rack => (
+                <Marker
+                    key={racks_user_locs.indexOf(rack)}
+                    position={{ lat: rack.rack_details.Lat, lng: rack.rack_details.Lng }}
+                    icon={{
+                        url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png",
+                        scaledSize: new window.google.maps.Size(30, 30),
+                        anchor: new window.google.maps.Point(15, 0)
+                    }}
+                    visible={rack.vis}
+                    onClick={() => {
+                        setSelectedUserRack(rack)
+                        setSelectedLTARack(null)
                         setSelectedShop(null)
                     }}
                 />
@@ -182,19 +276,32 @@ export function Main({ }) {
                     visible={shop.vis}
                     onClick={() => {
                         setSelectedShop(shop)
-                        setSelectedRack(null)
+                        setSelectedLTARack(null)
+                        setSelectedUserRack(null)
                     }}
                 />
             ))}
 
-            {/* Display info window for racks when marker selected */}
-            {selectedRack ? (<InfoWindow
-                position={{ lat: selectedRack.rack_details.geometry.coordinates[1], lng: selectedRack.rack_details.geometry.coordinates[0] }}
-                onCloseClick={() => { setSelectedRack(null); }}
+            {/* Display info window for LTA racks when marker selected */}
+            {selectedLTARack ? (<InfoWindow
+                position={{ lat: selectedLTARack.rack_details.geometry.coordinates[1], lng: selectedLTARack.rack_details.geometry.coordinates[0] }}
+                onCloseClick={() => { setSelectedLTARack(null); }}
             >
                 <div
-                    dangerouslySetInnerHTML={{ __html: selectedRack.rack_details.properties.Description }}
+                    dangerouslySetInnerHTML={{ __html: selectedLTARack.rack_details.properties.Description }}
                 >
+                </div>
+            </InfoWindow>) : null}
+
+            {/* Display info for user racks when marker selected */}
+            {selectedUserRack ? (
+            <InfoWindow
+                position={{ lat: selectedUserRack.rack_details.Lat, lng: selectedUserRack.rack_details.Lng }}
+                onCloseClick={() => {setSelectedUserRack(null)}}
+            >
+                <div>
+                    <b>User Added Rack</b>
+                    <p>Verified: {selectedUserRack.rack_details.Verified? "yes" : "no"}</p>
                 </div>
             </InfoWindow>) : null}
 
@@ -215,7 +322,13 @@ export function Main({ }) {
         <Router>
             <Drawer onSend={send_loc} panTo={panTo} markStart={markStart} markDest={markDest} />
         </Router>
-
+        <Router>
+            <Navbar 
+                setOverall={setOverall} 
+                setRepairVis={setRepairVis} 
+                setRackVis={setRackVis}/>
+        </Router>
+        
 
 
     </div>
@@ -324,7 +437,7 @@ function Search({ placeholder, setInput, panTo, markMap }) {
                     const results = await getGeocode({ address });
                     const { lat, lng } = await getLatLng(results[0]);
                     console.log(placeholder, { lat, lng });
-                    console.log(",\n\"Lat\":" + lat + ",\n\"Lng\":" + lng);
+                    // console.log(",\n\"Lat\":" + lat + ",\n\"Lng\":" + lng);
                     setInput({ lat, lng })
                     markMap({ lat, lng })
                     panTo({ lat, lng })
@@ -347,4 +460,32 @@ function Search({ placeholder, setInput, panTo, markMap }) {
             </Combobox>
         </div>
     );
+}
+
+function Navbar({ setOverall, setRepairVis, setRackVis }) {
+
+    function Home(){
+        alert("function not done")
+    }
+
+    return (
+        <div className='nav-bar'>
+            <button 
+            className='btn-home'
+            onClick={setOverall}>Home</button> 
+            <button 
+            className='btn-route'
+            onClick={Home}>Route</button> 
+            <button 
+            className='btn-repair'
+            onClick={setRepairVis}>Repair Shops</button> 
+            <button 
+            className='btn-racks'
+            onClick={setRackVis}>Racks</button> 
+            <button 
+            className='btn-add'
+            onClick={Home}>Add Racks</button>
+            
+        </div>
+    )
 }
